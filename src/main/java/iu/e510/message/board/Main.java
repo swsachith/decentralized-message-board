ba348d509cbe95c694a6d84f1b41ff732d72b1a1
swsachith <swsachith@gmail.com>
2019-04-05 11:39:25 -0400

    adding main temp

--- src/main/java/iu/e510/message/board/Main.java
+++ src/main/java/iu/e510/message/board/Main.java
@@ -1,7 +1,26 @@
 package iu.e510.message.board;
 
+import iu.e510.message.board.cluster.ClusterManager;
+import iu.e510.message.board.cluster.data.DataManager;
+import iu.e510.message.board.cluster.data.DataManagerImpl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello Folks!");
+        String ip = "192.168.7.5";
+        try {
+            ClusterManager clusterManager = new ClusterManager(ip);
+            DataManager dataManager = new DataManagerImpl(ip);
+            dataManager.addData("hapoi","hi");
+            dataManager.addData("IN","hi");
+            System.out.println();
+            System.out.println();
+            int i = 0;
+            while (i < Integer.MAX_VALUE) {
+                Thread.sleep(3000);
+                i++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
